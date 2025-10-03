da4c1027e8e95e6329e58f1b78531b375d5e862a
bernikr <git@berni.kr>
2025-10-03 17:59:33 +0200

    update version

--- sync.py
+++ sync.py
@@ -25,7 +25,8 @@ def main() -> None:
     lock_ruff_version = ruff_versions[0]
     print(f"Detected uv.lock ruff version: {lock_ruff_version}")
 
-    pc = yaml.safe_load(pre_commit_file.read_text(encoding="utf-8"))
+    pc_content = pre_commit_file.read_text(encoding="utf-8")
+    pc = yaml.safe_load(pc_content)
     ruff_versions = [
         repo["rev"] for repo in pc["repos"] if repo["repo"] == "https://github.com/astral-sh/ruff-pre-commit"
     ]
@@ -35,5 +36,21 @@ def main() -> None:
     if len(ruff_versions) > 1:
         print("Could not determine a unique ruff version from .pre-commit-config.yaml.")
         sys.exit(1)
-    pc_ruff_version = ruff_versions[0]
+    pc_ruff_version = ruff_versions[0].removeprefix("v")
     print(f"Detected pre-commit ruff version: {pc_ruff_version}")
+
+    if lock_ruff_version == pc_ruff_version:
+        print("Versions are already in sync.")
+        sys.exit(0)
+
+    pc_content_new = pc_content.replace(
+        f"repo: https://github.com/astral-sh/ruff-pre-commit\n    rev: v{pc_ruff_version}",
+        f"repo: https://github.com/astral-sh/ruff-pre-commit\n    rev: v{lock_ruff_version}",
+    )
+
+    if pc_content == pc_content_new:
+        print("Cant replace version in .pre-commit-config.yaml. Please do so manually.")
+        sys.exit(1)
+
+    print(f"Updating .pre-commit-config.yaml ruff version to {lock_ruff_version}")
+    pre_commit_file.write_text(pc_content_new, encoding="utf-8")
